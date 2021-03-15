b6241893cff006bf76ba6f46756840648848eb2d
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-03-14 18:46:46 -0700

    change path of API

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const myip = require('./myip.js');
 
 const PORT = process.env.PORT || 3167;
 
-app.use('/', require('./myip.js'));
+app.use('/api/myip', require('./myip.js'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
